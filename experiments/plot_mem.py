7ced77ba8c7485766a9b6717ead5eb560dba3d65
Antonios Karkatsoulis <tonykarkats@hotmail.com>
2017-03-22 20:08:51 +0100

    Added Azure measurements to rmem

--- experiments/plot_mem.py
+++ experiments/plot_mem.py
@@ -23,13 +23,15 @@ plt.show()
 
 
 # wmem
-wmem = np.arange(0, 10)
-fct_w = np.array([238, 235, 178, 44.31, 32.44, 20.73, 15.91, 9.9, 8.51, 8.51 ])
-vm_fct_w = np.array([313, 300, 216, 70, 64, 43.2, 34.5, 20.42, 12.1, 11.1])
-wmem_labels = ['512', '1k', '2k', '4k', '8k', '16k', '32k', '64k', '128k', '256k']
+wmem = np.arange(0, 11)
+fct_w = np.array([238, 235, 178, 44.31, 32.44, 20.73, 15.91, 9.9, 8.51, 8.51, 8.51 ])
+vm_fct_w = np.array([313, 300, 216, 70, 64, 43.2, 34.5, 20.42, 12.1, 11.1, 11.0])
+azure_fct_w = np.array([52.81, 44.57, 63.24, 38.27, 38.11, 38.55, 38.31, 20.74, 15.11, 10.68, 10.85])
+wmem_labels = ['512', '1k', '2k', '4k', '8k', '16k', '32k', '64k', '128k', '256k', '512k']
 
 plt.plot(wmem, fct_w, label='Bare Metal')
-plt.plot(wmem, vm_fct_w, label='VM')
+plt.plot(wmem, vm_fct_w, label='Dryad VM')
+plt.plot(wmem, azure_fct_w, label='Azure VM')
 plt.xticks(wmem, wmem_labels)
 plt.margins(0.2)
 plt.legend()
