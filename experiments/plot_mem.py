6c3e996f310a359c5d2b23628ed5f3071a45d3f9
Antonios Karkatsoulis <tonykarkats@hotmail.com>
2017-03-22 10:46:39 +0100

    Added VM measurements to rmem plot

--- experiments/plot_mem.py
+++ experiments/plot_mem.py
@@ -6,9 +6,11 @@ import numpy as np
 # rmem
 rmem = np.arange(0, 10)
 fct_r = np.array([13.75, 13.78, 13.01, 11.66, 11.01, 10.65, 8.8, 8.51, 8.51, 8.51])
+vm_fct_r = np.array([18.54, 16.44, 15.43, 14.3, 12.2, 11.7, 10.23, 10.23, 10.23])
 rmem_labels = ['512', '1k', '2k', '4k', '8k', '16k', '32k', '64k', '128k', '256k']
 
 plt.plot(rmem, fct_r)
+plt.plot(rmem, vm_fct_r)
 plt.xticks(rmem, rmem_labels)
 plt.margins(0.2)
 
